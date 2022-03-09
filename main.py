373365bf815c520ae2c3d5f829d2b12a7013237d
RodionK77 <K.Rodion77@gmail.com>
2022-03-09 17:58:33 +0300

    Share

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import numpy as np
 # s = [2,1,3,2,3,4,5] #2
 # print(len(set(s)))
 
-# s = [3, 2, 1]  # 3
-# for i in s[::-1]: print(i)
+#s = [3, 2, 1]  # 3
+#print([i for i in s[::-1]])
 
 #x = 4 #4
 #arr = [1, 4, 4, 5, 6, 4]
